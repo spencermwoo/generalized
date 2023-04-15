6bd76451854a82313eb9330be02bee2a14105164
spencermwoo <spencermwoo>
2023-04-15 00:19:39 -0400

    add /api/ and generalized API extensions

--- index.js
+++ index.js
@@ -13,30 +13,54 @@ const port = 3000
 
 URL_PATH = 'localhost:3000'
 
-html_snippet = `
+const html_snippet = `
 <!doctype html>
 <html>
 <head><title>Generalized API</title></head>
 <body>
   <h1>`
 
-prompt = `Create an HTML document with content on ${URL_PATH}.  In a separate section also add relative links to related topics
-
-` + html_snippet;
-
 app.get('/', (req, res) => {
-  html_home = `Welcome to the Generalized API</h1>
+  const html_home = `Welcome to the Generalized API</h1>
 <p>This site is a one-stop-shop for all your information needs.  We have information on <a href="/health">health</a>, <a href="/finance">finance</a>, <a href="/technology">technology</a>, <a href="/sports">sports</a>, and more</p>`;
 
   res.send(html_snippet + html_home)
 })
 
+// JSON API
+app.get('/api/*', async (req, res) => {
+  const CUR_PATH = req.originalUrl.slice(5)
+
+  res.send(await json(CUR_PATH))
+})
+
+app.get(/\w\.\w+$/, async (req, res) => {
+  const CUR_PATH = req.path.slice(1);
+  const {ct, body} = await generalized(CUR_PATH)
+
+  res.setHeader('Content-Type', ct)
+  res.send(body)
+})
+
+// HTML API
 app.get('*', async (req, res) => {
-  const CUR_PATH = req.originalUrl.split("/")[1];
+  const CUR_PATH = req.path.slice(1);
+  res.send(await html(CUR_PATH))
+})
+
+async function generalized(CUR_PATH){
+  const generalized_prompt = `Create a response document with content that matches the URL path: ${URL_PATH}
+
+The first line is the Content-Type of the response.
+The following lines is the returned data.
+In the case of an HTML response, also add relative links to related topics.
+
+Content-Type:
+`
 
   const {data} = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: prompt.replaceAll(URL_PATH, CUR_PATH),
+    prompt: generalized_prompt.replaceAll(URL_PATH, CUR_PATH),
     temperature: 0.7,
     max_tokens: 512,
     top_p: 1,
@@ -44,8 +68,54 @@ app.get('*', async (req, res) => {
     presence_penalty: 0,
   });
 
-  res.send(html_snippet + data.choices[0].text)
-})
+  const response = data.choices[0].text
+  const idx = response.indexOf("\n");
+
+  return {
+    "ct": response.substring(0, idx), 
+    "body": response.slice(idx)
+  }
+}
+
+async function json(CUR_PATH){
+  const json_snippet = `
+  {
+    "origin": "Generalized API",
+  `;
+
+  const json_prompt = `Create a JSON API response that matches ${URL_PATH}` + json_snippet;
+  res.setHeader('Content-Type', 'application/json')
+
+  const {data} = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: json_prompt.replaceAll(URL_PATH, CUR_PATH),
+    temperature: 0.7,
+    max_tokens: 512,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return json_snippet + data.choices[0].text
+}
+
+async function html(CUR_PATH){
+  const html_prompt = `Create a HTML document with content on ${URL_PATH}.  In a separate section also add relative links to related topics
+
+` + html_snippet;
+
+  const {data} = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: html_prompt.replaceAll(URL_PATH, CUR_PATH),
+    temperature: 0.7,
+    max_tokens: 512,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return html_snippet + data.choices[0].text
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
